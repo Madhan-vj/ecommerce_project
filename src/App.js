1386f15c5fa02d38e93274d8ccf6c97710efb00b
Madhan-vj <59563681+Madhan-vj@users.noreply.github.com>
2021-08-19 19:40:03 +0530

    pricecommit

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ import { CloseButton,Card,ListGroup,Container, Row, Col } from 'react-bootstrap'
 import './App.css';
 
 function App() {
-  const [data, setData] = useState("");
+  const [data, setData] = useState([]);
   const [price, setPrice] = useState([]);
   const [total, setTotal] = useState(0);
   useEffect(() => {
-  const fetchProduct = async() => {
-    try{
-      const res = await fetch("https://5cdd0a92b22718001417c19d.mockapi.io/api/products");
-      const json = await res.json();
-      console.log("json",json);
-      setData(json);
-    }
-    catch(err){
-      console.log("err",err);
-    }
-  }
+  
   fetchProduct();
 },[])
-
+const fetchProduct = async() => {
+  try{
+    const res = await fetch("https://5cdd0a92b22718001417c19d.mockapi.io/api/products");
+    const json = await res.json();
+    console.log("json",json);
+    setData(json);
+  }
+  catch(err){
+    console.log("err",err);
+  }
+}
   function handleClick (clickData) {
   // console.log('Click happened');
   // console.log(clickData);
@@ -30,11 +30,11 @@ function App() {
   setTotal(total + clickData.price);
   }
 
-  function handleClose(item){
+  function handleClose(item,index){
     // console.log("close clicked");
-    // console.log("itemzz",item);
-    const updatedPrice = price.filter(price => price.id !== item.id)
-    setPrice(updatedPrice);
+    // console.log("itemzz",item,index);
+    price.splice(index, 1);
+    setPrice(price);
     const updatedTotal = total - item.price;
     setTotal(updatedTotal);
   }
@@ -56,9 +56,10 @@ function App() {
             <Card style={{ width: '18rem', marginRight: "23px", marginTop: "50px"}}>
               <Card.Header>Prices</Card.Header>
               <ListGroup variant="flush">
+              {console.log("pricessss",price)}
                 {
-                price.map((item)=>(
-                  <ListGroup.Item  ><Row><Col>{item.productName}</Col><CloseButton onClick={() => handleClose(item)}/></Row><br/>Rs.{item.price}</ListGroup.Item>
+                price.map((item,index)=>(
+                  <ListGroup.Item  ><Row><Col>{item.productName}</Col><CloseButton onClick={() => handleClose(item,index)}/></Row><br/>Rs.{item.price}</ListGroup.Item>
                   ))
                 }
                 <ListGroup.Item>Total = Rs.{total}</ListGroup.Item>
